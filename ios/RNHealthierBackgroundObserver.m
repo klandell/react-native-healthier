5d6d6b313a1cb49d7e85e1e426c76ecdc35ebc8f
Kyle Landell <kyle.landell@mssm.edu>
2022-07-07 20:43:29 -0400

    Only notify of HK observation if there is no error

--- ios/RNHealthierBackgroundObserver.m
+++ ios/RNHealthierBackgroundObserver.m
@@ -1,5 +1,3 @@
-// https://gist.github.com/phatblat/654ab2b3a135edf905f4a854fdb2d7c8
-
 #import "RNHealthierBackgroundObserver.h"
 #import "RNHealthierObservationEmitter.h"
 #import "RNHealthierModule-Swift.h"
@@ -8,21 +6,22 @@
 
 - (void)initObservers {
     RNHealthierObservationEmitter *emitter = [RNHealthierObservationEmitter allocWithZone:nil];
-    
     NSArray* backgroundDelivery = [NSArray arrayWithArray:[[NSUserDefaults standardUserDefaults] objectForKey:@"RNHealthier_BackgroundDelivery"]];
-
     RNHealthierStore *store = [RNHealthierStore sharedInstance];
     
     for (NSString *dataTypeIdentifierString in backgroundDelivery) {
         [store observe:dataTypeIdentifierString completion:^(HKObserverQueryCompletionHandler _Nonnull completionHandler, NSError * _Nullable error) {
-            NSUUID *uuid = [NSUUID UUID];
-            [emitter notifyOfHKObservation:dataTypeIdentifierString observationUUID:uuid completionHandler:completionHandler];
+            if (!error) {
+                NSUUID *uuid = [NSUUID UUID];
+                [emitter notifyOfHKObservation:dataTypeIdentifierString observationUUID:uuid completionHandler:completionHandler];
+            } else {
+                completionHandler();
+            }
         }];
   
         // TODO: DON'T USE IMMEDIATE!, GET IT FROM USER DEFAULTS!!
         [store enableBackgroundDelivery:dataTypeIdentifierString updateFrequencyString:@"IMMEDIATE" completion:^(BOOL success, NSError * _Nullable error) {
             // TODO: IMPLEMENT ME!
-            NSLog(@"======= enable backgroungDelivery");
         }];
     }
 }
